70559dc8c16509bfa25ab823a5040db6de2c7885
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-03-04 00:08:38 -0600

    the current day is displayed at the top of the calendar

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,16 @@
-var today = moment().format('dddd MMMM Do YYYY, h:mm:ss a'); // March 3rd 2021, 11:42:58 pm
-console.log(today);
-$('#currentDay').text(today);
+function simpleCalendar() {
+    $(document).ready(function () {
+      let momentVar = moment().format('dddd, MMMM Do, YYYY h:mm a');
+      let workHours = ["9 a.m.", "10 a.m.", "11 a.m.", "12 p.m.", "1 p.m.", "2 p.m.", "3 p.m.", "4 p.m.",
+        "5 p.m."]
+        
+      //the current day is displayed at the top of the calendar
+      function currentDay() {
+        let currentDay = $("#currentDay").text(momentVar);
+        return currentDay;
+      }
+  
+      currentDay();
+      let row;
+      let timeCol;
+  
